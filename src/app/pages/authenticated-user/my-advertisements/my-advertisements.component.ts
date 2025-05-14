38731589f1d92e6365096453f8350dc72f57e5c4
Lucas <P0659387@portoseguro.com.br>
2025-05-14 17:40:54 -0300

    a

--- src/app/pages/authenticated-user/my-advertisements/my-advertisements.component.ts
+++ src/app/pages/authenticated-user/my-advertisements/my-advertisements.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-advertisements',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './my-advertisements.component.html',
   styleUrl: './my-advertisements.component.scss'
 })
