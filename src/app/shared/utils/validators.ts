045ed615bc22ebda0d7a1d1d52828f89b9b18ac4
Lucas <P0659387@portoseguro.com.br>
2025-05-28 09:53:56 -0300

    ajuste

--- src/app/shared/utils/validators.ts
+++ src/app/shared/utils/validators.ts
@@ -1,29 +1,52 @@
-import { FormControl, FormGroup } from "@angular/forms";
+import { AbstractControl, FormArray, FormControl, FormGroup } from "@angular/forms";
 
 export function isInvalid(formControl: FormControl): boolean {
     return formControl.invalid && formControl.touched;
 }
 
-export function scrollToInvalidField(formGroup: FormGroup) {
-    const invalidControls = Object.keys(formGroup.controls).filter(key => {
-        const control = formGroup.get(key);
-        return control && control.invalid && control.touched;
-    });
+export function scrollToInvalidField(formGroup: FormGroup | FormArray) {
+    const firstInvalidControlName = findFirstInvalidControl(formGroup);
 
-    if (invalidControls.length === 0) return;
+    if (!firstInvalidControlName) return;
 
-    const firstInvalid = invalidControls[0];
-
-    const el = document.querySelector(`[formControlName="${firstInvalid}"]`);
+    const el = document.querySelector(
+        `[formControlName="${firstInvalidControlName}"], 
+       [formGroupName="${firstInvalidControlName}"],
+       [formArrayName="${firstInvalidControlName}"]`
+    );
 
     if (el) {
         el.scrollIntoView({
             behavior: 'smooth',
             block: 'center',
-            inline: 'nearest'
+            inline: 'nearest',
         });
+
         setTimeout(() => {
             (el as HTMLElement).focus();
         }, 500);
     }
+}
+
+function findFirstInvalidControl(control: AbstractControl): string | null {
+    if (control instanceof FormGroup || control instanceof FormArray) {
+        for (const key of Object.keys(control.controls)) {
+            const child = control.get(key);
+
+            if (child) {
+                if (child.invalid && child.touched) {
+                    if (child instanceof FormControl) {
+                        return key;
+                    }
+
+                    const deeper = findFirstInvalidControl(child);
+                    return deeper ?? key;
+                } else if (child instanceof FormGroup || child instanceof FormArray) {
+                    const deeper = findFirstInvalidControl(child);
+                    if (deeper) return deeper;
+                }
+            }
+        }
+    }
+    return null;
 }
